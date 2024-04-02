fbc19ced36d8b7526882aa6abd2a46a3d4ef6bc6
Al Viro <viro@zeniv.linux.org.uk>
2024-04-02 00:28:28 -0400

    sparc32: make __cmpxchg_u32() return u32
    Conversion between u32 and unsigned long is tautological there,
    and the only use of return value is to return it from
    __cmpxchg() (which return unsigned long).
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- arch/sparc/include/asm/cmpxchg_32.h
+++ arch/sparc/include/asm/cmpxchg_32.h
@@ -39,7 +39,7 @@ static __always_inline unsigned long __arch_xchg(unsigned long x, __volatile__ v
 /* bug catcher for when unsupported size is used - won't link */
 void __cmpxchg_called_with_bad_pointer(void);
 /* we only need to support cmpxchg of a u32 on sparc */
-unsigned long __cmpxchg_u32(volatile u32 *m, u32 old, u32 new_);
+u32 __cmpxchg_u32(volatile u32 *m, u32 old, u32 new_);
 
 /* don't worry...optimizer will get rid of most of this */
 static inline unsigned long
