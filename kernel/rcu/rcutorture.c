47c20bacd14a111ab3c6d49975ea82e0993c9927
Paul E. McKenney <paulmck@kernel.org>
2024-07-03 22:22:55 -0700

    rcutorture: Avoid spurious RUDE01 failures
    On architectures with CONFIG_ARCH_HAS_NOINSTR_MARKINGS=y, the
    synchronize_rcu_tasks_rude() function is a no-op, and will thus result
    in RUDE01 failures.  This commit therefore substitutes synchronize_rcu()
    in that case, for which preemption-disabled regions of code also act
    as readers.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/rcutorture.c
+++ kernel/rcu/rcutorture.c
@@ -921,6 +921,14 @@ static struct rcu_torture_ops tasks_ops = {
  * Definitions for rude RCU-tasks torture testing.
  */
 
+static void torture_synchronize_rcu_tasks_rude(void)
+{
+	if (IS_ENABLED(CONFIG_ARCH_HAS_NOINSTR_MARKINGS))
+		synchronize_rcu();
+	else
+		synchronize_rcu_tasks_rude();
+}
+
 static struct rcu_torture_ops tasks_rude_ops = {
 	.ttype		= RCU_TASKS_RUDE_FLAVOR,
 	.init		= rcu_sync_torture_init,
@@ -928,8 +936,8 @@ static struct rcu_torture_ops tasks_rude_ops = {
 	.read_delay	= rcu_read_delay,  /* just reuse rcu's version. */
 	.readunlock	= rcu_torture_read_unlock_trivial,
 	.get_gp_seq	= rcu_no_completed,
-	.sync		= synchronize_rcu_tasks_rude,
-	.exp_sync	= synchronize_rcu_tasks_rude,
+	.sync		= torture_synchronize_rcu_tasks_rude,
+	.exp_sync	= torture_synchronize_rcu_tasks_rude,
 	.gp_kthread_dbg	= show_rcu_tasks_rude_gp_kthread,
 	.get_gp_data	= rcu_tasks_rude_get_gp_data,
 	.cbflood_max	= 50000,
