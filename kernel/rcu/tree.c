91a8aa4d911d00fe9dfdf43c63ae8fbfa226cda5
Paul E. McKenney <paulmck@kernel.org>
2024-08-16 14:22:48 -0700

    rcu: Permit start_poll_synchronize_rcu*() with interrupts disabled
    The header comment for both start_poll_synchronize_rcu() and
    start_poll_synchronize_rcu_full() state that interrupts must be enabled
    when calling these two functions, and there is a lockdep assertion in
    start_poll_synchronize_rcu_common() enforcing this restriction.  However,
    there is no need for this restrictions, as can be seen in call_rcu(),
    which does wakeups when interrupts are disabled.
    
    This commit therefore removes the lockdep assertion and the comments.
    
    Reported-by: Kent Overstreet <kent.overstreet@linux.dev>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -4193,7 +4193,6 @@ static void start_poll_synchronize_rcu_common(void)
 	struct rcu_data *rdp;
 	struct rcu_node *rnp;
 
-	lockdep_assert_irqs_enabled();
 	local_irq_save(flags);
 	rdp = this_cpu_ptr(&rcu_data);
 	rnp = rdp->mynode;
@@ -4218,9 +4217,6 @@ static void start_poll_synchronize_rcu_common(void)
  * grace period has elapsed in the meantime.  If the needed grace period
  * is not already slated to start, notifies RCU core of the need for that
  * grace period.
- *
- * Interrupts must be enabled for the case where it is necessary to awaken
- * the grace-period kthread.
  */
 unsigned long start_poll_synchronize_rcu(void)
 {
@@ -4241,9 +4237,6 @@ EXPORT_SYMBOL_GPL(start_poll_synchronize_rcu);
  * grace period (whether normal or expedited) has elapsed in the meantime.
  * If the needed grace period is not already slated to start, notifies
  * RCU core of the need for that grace period.
- *
- * Interrupts must be enabled for the case where it is necessary to awaken
- * the grace-period kthread.
  */
 void start_poll_synchronize_rcu_full(struct rcu_gp_oldstate *rgosp)
 {
