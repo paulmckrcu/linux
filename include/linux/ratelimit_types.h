1dcdee6e4ad11a039638f6251a9b6f50252dd311
Paul E. McKenney <paulmck@kernel.org>
2025-04-07 09:19:37 -0700

    ratelimit: Change ___ratelimit() return value to bool
    This function always returns true or false, therefore make the return
    type match this reality.
    
    Suggested-by: Mateusz Guzik <mjguzik@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Kuniyuki Iwashima <kuniyu@amazon.com>
    Cc: Mateusz Guzik <mjguzik@gmail.com>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: John Ogness <john.ogness@linutronix.de>
    Cc: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Jon Pan-Doh <pandoh@google.com>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Karolina Stolarek <karolina.stolarek@oracle.com>

--- include/linux/ratelimit_types.h
+++ include/linux/ratelimit_types.h
@@ -43,7 +43,7 @@ struct ratelimit_state {
 	struct ratelimit_state name =					\
 		RATELIMIT_STATE_INIT(name, interval_init, burst_init)	\
 
-extern int ___ratelimit(struct ratelimit_state *rs, const char *func);
+extern bool ___ratelimit(struct ratelimit_state *rs, const char *func);
 #define __ratelimit(state) ___ratelimit(state, __func__)
 
 #endif /* _LINUX_RATELIMIT_TYPES_H */
