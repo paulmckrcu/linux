eb438afa1d03cf8333c48061ecba8b16bf009cb8
Paul E. McKenney <paulmck@kernel.org>
2026-03-19 14:41:22 -0700

    squash! rcutorture: Test call_srcu() with preemption disabled and not
    Acquire pi lock rather than just disabling preemption.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/rcutorture.c
+++ kernel/rcu/rcutorture.c
@@ -842,13 +842,14 @@ static unsigned long srcu_torture_completed(void)
 
 static void srcu_torture_deferred_free(struct rcu_torture *rp)
 {
-	bool pd = jiffies & 0x1;
+	unsigned long flags;
+	bool lockit = jiffies & 0x1;
 
-	if (pd)
-		preempt_disable();
+	if (lockit)
+		raw_spin_lock_irqsave(&current->pi_lock, flags);
 	call_srcu(srcu_ctlp, &rp->rtort_rcu, rcu_torture_cb);
-	if (pd)
-		preempt_enable();
+	if (lockit)
+		raw_spin_unlock_irqrestore(&current->pi_lock, flags);
 }
 
 static void srcu_torture_synchronize(void)
