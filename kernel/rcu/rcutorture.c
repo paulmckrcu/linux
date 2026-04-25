d32c9e9c26e537f450fe0c1ed9b8bf56fc8f05ac
Paul E. McKenney <paulmck@kernel.org>
2026-04-25 10:50:57 -0700

    rcutorture: Check for immediate deboosting at reader end
    This commit adds a check for failure to have fully deboosted a
    multi-segmented RCU reader at the end of the full read-side critical
    section.  This check only happens for fully task-level readers, because a
    a handler might have interrupted an already-boosted task-level RCU reader,
    and a reader in that handler could then cause false positives.  The first
    failed check (due to an RCU reader that was not immediately deboosted)
    causes a splat and a list of the segments making up that RCU reader.
    Subsequent failures fail silently, all in the name of keeping console
    output down to a dull roar.
    
    Although most uses of RCU priority boosting serve as debugging aids,
    this might change, and in fact might already have changed.  And allowing
    (for example) RCU priority boosting to persist until the next scheduler
    tick could cause an aggressively real-time system to miss sub-millisecond
    deadlines.  So we do need to find this sort of problem during testing,
    and preferably not in the field.
    
    The name and type of the newly added rcu_torture_ops function pointer
    (named "->is_task_rcu_boosted()") may need to change should other
    end-of-reader checks be needed.  But let's start simple.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/rcutorture.c
+++ kernel/rcu/rcutorture.c
@@ -426,6 +426,7 @@ struct rcu_torture_ops {
 	void (*format_gp_seqs)(unsigned long long seqs, char *cp, size_t len);
 	void (*set_gpwrap_lag)(unsigned long lag);
 	int (*get_gpwrap_count)(int cpu);
+	bool (*is_task_rcu_boosted)(void);
 	long cbflood_max;
 	int irq_capable;
 	int can_boost;
@@ -635,6 +636,7 @@ static struct rcu_torture_ops rcu_ops = {
 	.format_gp_seqs		= rcutorture_format_gp_seqs,
 	.set_gpwrap_lag		= rcu_set_gpwrap_lag,
 	.get_gpwrap_count	= rcu_get_gpwrap_count,
+	.is_task_rcu_boosted	= rcu_is_task_rcu_boosted,
 	.irq_capable		= 1,
 	.can_boost		= IS_ENABLED(CONFIG_RCU_BOOST),
 	.extendables		= RCUTORTURE_MAX_EXTEND,
@@ -2580,7 +2582,9 @@ static void rcu_torture_one_read_end(struct rcu_torture_one_read_state *rtorsp,
  */
 static bool rcu_torture_one_read(struct torture_random_state *trsp, long myid)
 {
+	static int firsttime = 1;
 	int newstate;
+	unsigned int nsegs;
 	struct rcu_torture_one_read_state rtors;
 
 	WARN_ON_ONCE(!rcu_is_watching());
@@ -2592,6 +2596,14 @@ static bool rcu_torture_one_read(struct torture_random_state *trsp, long myid)
 		return false;
 	rtors.rtrsp = rcutorture_loop_extend(&rtors.readstate, trsp, rtors.rtrsp);
 	rcu_torture_one_read_end(&rtors, trsp);
+	if (WARN_ON_ONCE(cur_ops->is_task_rcu_boosted && cur_ops->is_task_rcu_boosted() &&
+			 !in_serving_softirq()) &&
+	    READ_ONCE(firsttime) && xchg(&firsttime, 0)) {
+		nsegs = rtors.rtrsp - rtors.rtseg;
+		nsegs = clamp_val(nsegs, 0, RCUTORTURE_RDR_MAX_SEGS);
+		pr_alert("Slow-deboost rcutorture reader segments:\n");
+		rcu_torture_dump_read_segs(rtors.rtseg, nsegs);
+	}
 	return true;
 }
 
