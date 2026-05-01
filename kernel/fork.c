bc59fcfa3583660e5b9117a2da9e0a7a2f0bf881
Paul E. McKenney <paulmck@kernel.org>
2026-05-01 15:35:36 -0700

    rcu: Add fields for blocked tasks with deferred quiescent states
    This commit adds a ->dqs_blkd_tasks list to the rcu_node structure,
    which will contain tasks that were preempted in a rcu_read_lock()
    segment of the current segmented RCU reader.  If a later rcu_read_lock()
    segment of the current segmented RCU reader is also preempted, then the
    corresponding task would be moved back to the ->blkd_tasks list.
    
    This also requires a ->rcu_node_entry_dqs field in the task structure,
    which records whether the corresponding task is on the ->blkd_tasks
    list (0), the ->dqs_blkd_tasks list (1), or neither (-1).
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1823,6 +1823,7 @@ static inline void rcu_copy_process(struct task_struct *p)
 	p->rcu_read_unlock_special.s = 0;
 	p->rcu_blocked_node = NULL;
 	INIT_LIST_HEAD(&p->rcu_node_entry);
+	p->rcu_node_entry_dqs = -1;
 #endif /* #ifdef CONFIG_PREEMPT_RCU */
 #ifdef CONFIG_TASKS_RCU
 	p->rcu_tasks_holdout = false;
