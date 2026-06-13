bbe72db5f0accb150de2cbcbb1810be03cd8d1a0
Paul E. McKenney <paulmck@kernel.org>
2026-06-13 13:18:08 -0700

    fixup! hazptrtorture: Add kthread to release deferred hazard pointers
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/hazptrtorture.c
+++ kernel/rcu/hazptrtorture.c
@@ -642,12 +642,14 @@ hazptr_torture_print_module_parms(struct hazptr_torture_ops *cur_ops, const char
 {
 	pr_alert("%s" TORTURE_FLAG
 		 "--- %s: nreaders=%d "
+		 "kthread_do_pending_ms=%d "
 		 "onoff_interval=%d onoff_holdoff=%d "
 		 "preempt_duration=%d preempt_interval=%d "
 		 "reader_sleep_us=%d "
 		 "shuffle_interval=%d shutdown_secs=%d stat_interval=%d stutter=%d "
 		 "verbose=%d\n",
 		 torture_type, tag, nrealreaders,
+		 kthread_do_pending_ms,
 		 onoff_interval, onoff_holdoff,
 		 preempt_duration, preempt_interval,
 		 reader_sleep_us,
