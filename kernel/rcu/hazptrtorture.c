89a2f7ecfaff1b180a7ae3f656239262595506fc
Paul E. McKenney <paulmck@kernel.org>
2026-06-13 14:59:40 -0700

    fixup! torture: Add a hazptrtorture.c torture test
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/hazptrtorture.c
+++ kernel/rcu/hazptrtorture.c
@@ -36,7 +36,6 @@ torture_param(int, kthread_do_pending_ms, -1,
 	      "Delay between cleanups for deferred hazard pointers (ms), zero to disable");
 // @@@ torture_param(int, leakpointer, 0, "Leak pointer dereferences from readers");
 torture_param(int, nreaders, -1, "Number of hazard-pointer reader threads");
-// @@@ Does testing CPU hotplug make sense for hazard pointers?
 torture_param(int, onoff_holdoff, 0, "Time after boot before CPU hotplugs (s)");
 torture_param(int, onoff_interval, 0, "Time between CPU hotplugs (jiffies), 0=disable");
 // @@@ Move the rcu_torture_preempt() function and friends to kernel/torture.c.
@@ -802,6 +801,10 @@ static int __init hazptr_torture_init(void)
 	}
 
 	firsterr = torture_create_kthread(hazptr_torture_writer, NULL, writer_task);
+	if (torture_init_error(firsterr))
+		goto unwind;
+
+	firsterr = torture_onoff_init(onoff_holdoff * HZ, onoff_interval, NULL);
 	if (torture_init_error(firsterr))
 		goto unwind;
 
