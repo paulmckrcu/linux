e5ee2ea2d00ae5a0ca53ccf9c9100f2357e1bd08
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2026-07-07 14:34:42 -0700

    hazptrtorture: Detach deferred and IPIed hazard pointers
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/hazptrtorture.c
+++ kernel/rcu/hazptrtorture.c
@@ -411,6 +411,7 @@ hazptr_torture_reader_tail(struct hazptr_pending *hppp, struct torture_random_st
 	preempt_enable();
 	if (irq_release && !(torture_random(trsp) % irq_release)) {
 		guard(preempt)();
+		hazptr_detach_from_task(&hppp->hpp_hc);
 		cpu = cpumask_next_wrap(smp_processor_id(), cpu_online_mask);
 		smp_call_function_single(cpu, hazptr_torture_release, hppp, 1);
 	} else {
@@ -429,6 +430,7 @@ static void hazptr_torture_defer(struct hazptr_pending *hppp, struct torture_ran
 
 	hazptr_detach_from_task(&hppp->hpp_hc);
 	guard(preempt)();
+	hazptr_detach_from_task(&hppp->hpp_hc);
 	cpu = cpumask_next_wrap(cpu, cpu_online_mask);
 	llhp = per_cpu_ptr(&hazptr_pending, cpu);
 	llist_add(&hppp->hpp_node, llhp);
