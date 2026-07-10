4c39c671c0fe89c420b3c008eb8e6f4a7bcac466
Paul E. McKenney <paulmck@kernel.org>
2026-07-10 12:04:59 -0700

    fixup! hazptr: Upgrade kernel-doc headers

--- include/linux/hazptr.h
+++ include/linux/hazptr.h
@@ -132,16 +132,15 @@ void hazptr_promote_to_backup_slot(struct hazptr_ctx *ctx, struct hazptr_slot *s
  * @ctx: The hazard-pointer context to be migrated.
  *
  * By default, a given hazptr_acquire() and the corresponding
- * hazptr_release() must run in a single execution context, for example, the
- * context of a single task or a single interrupt handler.  Please note that
- * things like interrupt handlers execute in the context of the interrupted
- * task.  When you have acquired a hazard pointer in one task and need
- * to release it in another, you must invoke hazptr_detach_from_task()
- * on that hazard pointer's context.  It is permissible to invoke
- * hazptr_detach_from_task() multiple times on the same @ctx while it is
- * protecting the same pointer, however, the first invocation absolutely
- * must be in the context of the task that did the hazptr_acquire(),
- * and must take place after the return from that hazptr_acquire().
+ * hazptr_release() must run in a single execution context, for example,
+ * the context of a single task or a single interrupt handler.  When you
+ * have acquired a hazard pointer in one context and need to release it
+ * in another, you must invoke hazptr_detach_from_task() on that hazard
+ * pointer's context.  It is permissible to invoke hazptr_detach_from_task()
+ * multiple times on the same @ctx while it is protecting the same pointer,
+ * however, the first invocation absolutely must be in the same context
+ * that did the hazptr_acquire(), and must take place after the return
+ * from that hazptr_acquire().
  *
  * For example, if a hazard pointer is acquired by a task and
  * released by a timer handler, that task would need to pass the hazard
@@ -191,16 +190,15 @@ void hazptr_note_context_switch(void)
  *
  * Load @addr_p, and protect the loaded pointer with hazard pointer.
  * This protection is roughly similar to that of a reference counter, and
- * ends with a hazptr_release().
+ * ends with a later call to hazptr_release().
  *
  * By default, the call to hazptr_release() must be running in the same
- * execution context that executed the corresponding hazptr_acquire(), for
- * example, within the same task or interrupt handler.  When it is necessary
- * to instead call hazptr_release() from the context of some other task,
- * pass @ctx to hazptr_detach_from_task() after invoking hazptr_acquire()
- * but before making the hazard pointer available to that other task.
- * Please note that things like interrupt handlers execute in the context
- * of the interrupted task.
+ * execution context as the corresponding hazptr_acquire(), for example,
+ * within the same task or interrupt handler.  When it is necessary
+ * to instead call hazptr_release() from some other context, pass @ctx
+ * to hazptr_detach_from_task() in the original context after invoking
+ * hazptr_acquire() but before making the hazard pointer available to that
+ * other context.
  *
  * It is not permissible to invoke hazptr_acquire() twice on the same @ctx
  * without an intervening hazptr_release().
@@ -283,12 +281,10 @@ static inline void hazptr_release_debug(struct hazptr_ctx *ctx, void *addr) { }
  * that invoked the corresponding hazptr_acquire(), for example, within the
  * same task or the same interrupt handler.  However, if this restriction
  * is problematic for your use case, please see hazptr_detach_from_task().
- * Please note that things like interrupt handlers execute in the context
- * of the interrupted task.
  *
- * It is permissible (though likely unwise from a maintainability
- * viewpoint) to invoke hazptr_release() twice on the same @ctx without
- * an intervening hazptr_acquire().
+ * It is permissible (though unwise from a maintainability viewpoint)
+ * to invoke hazptr_release() twice on the same @ctx without an intervening
+ * hazptr_acquire().
  */
 static inline
 void hazptr_release(struct hazptr_ctx *ctx, void *addr)
