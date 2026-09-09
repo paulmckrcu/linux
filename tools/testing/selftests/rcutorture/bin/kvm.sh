bab62e34126456663f64f528e27754042dbcd568
Paul E. McKenney <paulmck@kernel.org>
2026-09-09 15:27:57 -0700

    torture.sh: Add hazptr torturing
    This commit adds hazard-pointer torturing to the torture.sh script
    as a default-on option.  This uses the same duration as rcutorture.
    While in the area, change kvm.sh to tolerate the empty-string arguments
    that torture.sh can generate, for example, if a given test needs no
    boot arguments.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Boqun Feng <boqun@kernel.org>
    Cc: Bradley Morgan <brads@mainlining.org>

--- tools/testing/selftests/rcutorture/bin/kvm.sh
+++ tools/testing/selftests/rcutorture/bin/kvm.sh
@@ -98,6 +98,7 @@ usage () {
 
 while test $# -gt 0
 do
+	echo Argument: :$1:
 	case "$1" in
 	--allcpus)
 		cpus=$TORTURE_ALLOTED_CPUS
@@ -270,6 +271,8 @@ do
 		;;
 	--trust-make)
 		TORTURE_TRUST_MAKE="y"
+		;;
+	"")
 		;;
 	*)
 		echo Unknown argument $1
